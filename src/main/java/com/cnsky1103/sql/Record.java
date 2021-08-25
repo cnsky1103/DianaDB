4cf390e8ce143710d6bfdf88f9b1a5f73856dba6
cnsky1103 <cnsky1103@gmail.com>
2021-08-25 14:32:14 +0800

    update .gitignore

--- src/main/java/com/cnsky1103/sql/Record.java
+++ src/main/java/com/cnsky1103/sql/Record.java
@@ -11,15 +11,3 @@ public class Record {
         values.set(index, v);
     }
 }
-
-class Value {
-    Syntax.Type type;
-    int vINT;
-    double vDOUBLE;
-    String vString;
-
-    @Override
-    public String toString() {
-        return type == Type.INT ? String.valueOf(vINT) : (type == Type.DOUBLE ? String.valueOf(vDOUBLE) : vString);
-    }
-}
