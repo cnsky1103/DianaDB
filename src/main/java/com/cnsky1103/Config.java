9c4aba0fc9681d67ebbcc161d8221c91e1de396b
cnsky1103 <cnsky1103@gmail.com>
2021-08-25 16:38:02 +0800

    改Settings为Config

---
+++ src/main/java/com/cnsky1103/Config.java
@@ -0,0 +1,7 @@
+package com.cnsky1103;
+
+@Getter
+@Setter
+public final class Config {
+    private static String dataPath = "D:/Programming/FriedDB/frieddb/data/";
+}
--- src/main/java/com/cnsky1103/Settings.java
+++
@@ -1,5 +0,0 @@
-package com.cnsky1103;
-
-public final class Settings {
-    public static String dataPath = "D:/Programming/FriedDB/frieddb/data/";
-}
