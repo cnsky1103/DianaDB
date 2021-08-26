0bd865d6f900b902f0ee2b6e68522d99a9a78172
cnsky1103 <cnsky1103@gmail.com>
2021-08-26 20:46:56 +0800

    更改了配置的写法

--- src/main/java/com/cnsky1103/Config.java
+++ src/main/java/com/cnsky1103/Config.java
@@ -1,10 +1,5 @@
 package com.cnsky1103;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public final class Config {
-    private static String dataPath = "D:/Programming/FriedDB/frieddb/data/";
+    private static final String dataPath = "D:/Programming/FriedDB/frieddb/data/";
 }
