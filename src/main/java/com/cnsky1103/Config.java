41766328ba03e7f2f27514971053cfcdeabf8167
cnsky1103 <cnsky1103@gmail.com>
2021-08-30 17:26:19 +0800

    增加了.rec后缀

--- src/main/java/com/cnsky1103/Config.java
+++ src/main/java/com/cnsky1103/Config.java
@@ -3,4 +3,5 @@ package com.cnsky1103;
 public final class Config {
     public static final String dataPath = "~/FriedDB/data";
     public static final String tablesDataPath = "~/FriedDB/data/tables.data";
+    public static final String recordSuffix = ".rec";
 }
