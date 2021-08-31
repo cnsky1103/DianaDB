6e22bc3eb084f9f6b81d5a0b12c75dee18915a26
cnsky1103 <cnsky1103@gmail.com>
2021-08-31 15:58:46 +0800

    配置修改

--- src/main/java/com/cnsky1103/Config.java
+++ src/main/java/com/cnsky1103/Config.java
@@ -1,9 +1,7 @@
 package com.cnsky1103;
 
-import java.io.File;
-
 public final class Config {
-    public static final String dataPath = "~/FriedDB/data/";
+    public static final String dataPath = "/root/FriedDB/data/";
     public static final String tablesDataPath = dataPath + "tables.data";
     public static final String recordSuffix = ".rec";
 }
